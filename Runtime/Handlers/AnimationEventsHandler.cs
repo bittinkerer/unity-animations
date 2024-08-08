89aa4cf6bd96c3c8cdb2c5f22d942e9263550ab0
bittinkerer <bittinkerer@gmail.com>
2024-08-07 22:24:24 -0700

    Use GO for animation events for sender

--- Runtime/Handlers/AnimationEventsHandler.cs
+++ Runtime/Handlers/AnimationEventsHandler.cs
@@ -5,5 +5,5 @@ using Packages.Estenis.GameEvent_;
 public class AnimationEventsHandler : EventMonoBehaviour
 {
     public void OnAnimationEvent(GameEventObject gameEvent) =>
-        gameEvent.Raise(EventId, this, null);
+        gameEvent.Raise(EventId, this.gameObject, this.gameObject);
 }
